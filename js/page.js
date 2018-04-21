39f14218ffded4f197ed8d78ebc26a14d617b7c9
Ken Clements <kclements@techtonicgroup.com>
2018-04-21 08:50:09 -0600

    Stared work with bootstrap datatables

--- js/page.js
+++ js/page.js
@@ -3,11 +3,37 @@ var lib = new library("one");
 
 $(document).ready( function() {
     lib.getLibraryFromLocalStorage();
-    lib.books.forEach( function(book) {
-        console.log(book);
+    // lib.books.forEach( function(book) {
+    //     console.log(book);
+    // });
+
+    $("#displayTable").DataTable( {
+        data: this.books,
+        columns: [
+            { data: "author" },
+            { data: "title" },
+            { data: "numPages" },
+            { data: "pubDate"}
+        ]
     });
-    $("#displayTable").DataTable();
     var _this = this;
+
+    function createBookDisplayTable () {
+        var bookCount = 0;
+        var nodeClass = ".dataRow0";
+        var originalDataRow = $(nodeClass);
+        var tableRow = originalDataRow.cloneNode(true);
+        lib.books.forEach( function (book) {
+            tableRow.children("author").data = book.author;
+            tableRow.children("title").data = book.title;
+            tableRow.children("numPages").data = book.numPages;
+            tableRow.children("pubDate").data = book.pubDate.toDateString;
+            $(".tableBody").append(tableRow);
+            tableRow = originalDataRow.cloneNode(true);
+        });
+
+        return true;
+    }
 
     $("#formAddABook").on("submit", function() {
         var book;
