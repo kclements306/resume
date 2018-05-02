a7c566b6f8ccd7e1635798f732680ce558290de0
Ken Clements <kclements@techtonicgroup.com>
2018-05-02 14:10:05 -0600

    Completed refactor to ECMAScript 6.  Moved all code into library.js and removed page.js.

--- js/library.js
+++ js/library.js
@@ -1,9 +1,6 @@
-export class Library {}
-export class Book {}
-
 class Library {
 
-    constructor(libarayName) {
+    constructor(libraryName) {
         this.libraryName = libraryName;
         this.books = [];
     }
@@ -264,17 +261,201 @@ class Library {
         }
         return results;
     }
-
 }
 
 // Book object
-class Book(args) {
+class Book {
 
-    constructor {
+    constructor(args) {
         this.title = args.title;
         this.author = args.author;
         this.numPages = args.numPages;
         this.pubDate = new Date(args.pubDate);
     }
+}
+
+class Page extends Library {
+    constructor(libraryName) {
+        super(libraryName);
+        this.booksToAdd = [];
+    }
+
+    // get library from local storage and bind events.
+    // homeTable and addTable are here to make sure lib is populated first.
+    init() {
+        this.getLibraryFromLocalStorage();
+        this._bindEvents();
+
+        // This datatable is displayed on the home page
+        this.homeTable = $("#displayTable").DataTable({
+            data: this.books,
+            columns: [
+                { data: "author" },
+                { data: "title" },
+                { data: "numPages" },
+                { data: "pubDate",
+                    render: function (data) {
+                        return data.toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric"
+                        });
+                    }
+                },
+                {   orderable: false,
+                    data: "icons",
+                    render: function () {
+                        return ("<i class=\"material-icons editRow mx-2\">create</i> " + "<i class=\"material-icons deleteRow\">delete</i>");
+                    }
+                }
+            ]
+        });
+
+        // The add datatable is used when adding boooks it's inside the modal addModal
+        this.addTable = $("#addModalTable").DataTable({
+
+        });
+    }
+
+    _bindEvents() {
+        $("#addModal").on("show.bs.modal", $.proxy(this.modalAdd, this));
+        $("#btnAddABook").on("click", $.proxy(this.btnAddBookToList, this));
+        $("#btnSaveBooks").on("click", $.proxy(this.btnSaveBooksToLibrary, this));
+        $("#btnAddABook").on("click", $.proxy(this.btnAddBook, this));
+        $("#btnSaveEdit").on("click", $.proxy(this.btnSaveEdit, this));
+        $("#showAllAuthorsModal").on("click", ".allAuthorsList", $.proxy(this.btnAuthorToDelete, this));
+        $("#displayTable").on("click", ".deleteRow", "i", $.proxy(this.iconDeleteRow, this));
+        $("#displayTable").on("click", ".editRow", "i", $.proxy(this.iconEditAuthorAndTitle, this));
+        $("#showAllAuthorsModal").on("show.bs.modal", $.proxy(this.modalShowAllAuthors, this));
+        $("#recommendModal").on("show.bs.modal", $.proxy(this.modalRecommend, this));
+    }
+
+    // Show the modal to add books.
+    modalAdd() {
+
+    }
+
+    // This function takes the values from the input elements in the addModal modal and saves them to the booksToAdd array
+    // then it adds the book to the addModal table to display.
+    btnAddBookToList() {
+        // save the values that were inputted
+        var author = $(".inpAuthor").val();
+        var title = $(".inpTitle").val();
+        var numPages = $(".inpNumPages").val();
+        var pubDate = $(".inpPubDate").val();
+        this.booksToAdd.push(new Book({
+            author: author,
+            title: title,
+            numPages: numPages,
+            pubDate: pubDate
+        }));
+        // clear the input tags to show the placeholder value
+        $(".inpAuthor").val("");
+        $(".inpTitle").val("");
+        $(".inpNumPages").val("");
+        $(".inpPubDate").val("");
+        // add the book to the modal table
+        this.addTable.row.add([author, title, numPages, pubDate]);
+        this.addTable.draw(false);
+    }
+
+    // Save all books in booksToAdd array to the library
+    btnSaveBooksToLibrary() {
+        if (Array.isArray(this.booksToAdd) && this.booksToAdd.length) { // make sure there are books to add
+            this.addBooks(this.booksToAdd); //add the new books to the library
+            this.saveLibraryToLocalStorage();
+            // clear the table and the input fields in the add modal
+            this.addTable.clear();
+            $(".inpAuthor").val("");
+            $(".inpTitle").val("");
+            $(".inpNumPages").val("");
+            $(".inpPubDate").val("");
+            $("#addModal .close").click();
+            location.reload();
+        }
+    }
+
+    // This modal will show all unique authors in the library and allow users to delete all books by an
+    // author by clicking on their name
+    modalShowAllAuthors() {
+        $("#allAuthorsList").empty(); // clear out any old html
+        var insertString = "";
+        var authors = this.getAuthors();
+        authors.forEach(function (author) {
+            insertString = insertString + "<li class=\"allAuthorsList\">" + author + "</li>";
+        });
+        $("#allAuthorsList").append(insertString);
+    }
+
+    // Get a random book to recommend
+    modalRecommend() {
+        var book = this.getRandomBook();
+        $("#imgRecommendImage").attr("src", "images/" + book.title + ".jpg");
+        $("#pRecommendTitle").text(book.title);
+        $("#pRecommendAuthor").text(book.author);
+        $("#pRecommendNumPages").text(book.numPages);
+        $("#pRecommendPubDate").text(book.pubDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        }));
+    }
+
+    // After finishing editing the book title and/or author save the book to the library and 
+    // then save the library to local storage
+    btnSaveEdit() {
+        var author = $("#editModalAuthor").val();
+        var title = $("#editModalTitle").val();
+        var originalTitle = $("#editModalTitle").attr("name");
+        var books = this.getBookByTitle(originalTitle);
+        var book = books[0];
+        book.author = author;
+        book.title = title;
+        this.removeBookByTitle(originalTitle);
+        this.addBook(book);
+        this.saveLibraryToLocalStorage();
+        // this.homeTable.draw(false);
+        // $("#editModal").modal("hide");
+        location.reload();
+    }
+
+    btnAuthorToDelete(e) {
+
+        var author = $(e.currentTarget).text();
+        if (confirm("Are you sure you want to delete all the books by " + author + " ?")) {
+            this.removeBooksByAuthor(author);
+            this.saveLibraryToLocalStorage();
+        }
+        location.reload();
+    }
+
+    // edit Author and/or Title in the selected row
+    iconEditAuthorAndTitle(e) {
+        var tableRow = $(e.currentTarget).parent().parent("tr");
+        var author = tableRow.children("td:nth-child(1)").text();
+        var originalTitle = tableRow.children("td:nth-child(2)").text();
+        $("#editModalAuthor").text(author);
+        $("#editModalTitle").text(originalTitle);
+        $("#editModalTitle").attr("name", originalTitle); // save the title
+        $("#editModal").modal("show"); // pop the edit modal
+        this.homeTable.draw(false);
+    }
+
+    // delete the selected row from the table and the corresponding book from the library
+    iconDeleteRow(e) {
+        var tableRow = $(e.currentTarget).parent().parent("tr");
+        var title = tableRow.children("td:nth-child(2)").text();
+        if (confirm("Are you sure you want to delete \"" + title + "\" ?")) {
+            this.removeBookByTitle(title);
+            this.saveLibraryToLocalStorage();
+            this.homeTable.row(tableRow).remove();
+            this.homeTable.draw(false);
+        }
+    }
 
 }
+
+$(function () {
+    window.page = new Page("Library");
+    window.page.init();
+});
