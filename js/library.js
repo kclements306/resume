ff4c9164cba1e0ad767af05e14d01ce035259efa
Ken Clements <kclements@techtonicgroup.com>
2018-05-24 09:35:05 -0600

    The delete all books by authors is now working

--- js/library.js
+++ js/library.js
@@ -1,3 +1,4 @@
+
 // Book object
 class Book {
     constructor(args) {
@@ -24,11 +25,12 @@ class Library {
         init - get library and bind events.  homeTable and addTable are here to make sure lib is populated first.
     */
     init() {
-        this.getLibraryFromDB();
+        this.getAllBooksFromLibrary();
         this._bindEvents();
         // This datatable is displayed on the home page
         this.homeTable = $("#displayTable").DataTable({
             data: this.books,
+            pageLength: 25,
             // ajax: {
             //     dataType: "json",
             //     type: "GET",
@@ -59,7 +61,7 @@ class Library {
             ]
         });
 
-        // The add datatable is used when adding boooks it's inside the modal addModal
+        // The add datatable is used when adding books 
         this.addTable = $("#addModalTable").DataTable({
             paging: false,
             searching: false,
@@ -72,7 +74,7 @@ class Library {
         $("#btnSaveBooks").on("click", $.proxy(this.btnSaveBooksToLibrary, this));
         $("#btnAddABook").on("click", $.proxy(this.btnAddBook, this));
         $("#btnSaveEdit").on("click", $.proxy(this.btnSaveEdit, this));
-        $("#showAllAuthorsModal").on("click", ".allAuthorsList", $.proxy(this.btnAuthorToDelete, this));
+        $("#showAllAuthorsModal").on("click", ".allAuthorsList", $.proxy(this.removeBooksByAuthor, this));
         $("#displayTable").on("click", ".deleteRow", "i", $.proxy(this.iconDeleteRow, this));
         $("#displayTable").on("click", ".editRow", "i", $.proxy(this.iconEditAuthorAndTitle, this));
         $("#showAllAuthorsModal").on("show.bs.modal", $.proxy(this.modalShowAllAuthors, this));
@@ -81,7 +83,7 @@ class Library {
 
     // Show the modal to add books.
     modalAdd() {
-
+        this.booksToAdd = [];    // make sure to start with no books
     }
 
     /*
@@ -95,41 +97,30 @@ class Library {
         let numPages = $(".inpNumPages").val();
         let pubDate = $(".inpPubDate").val();
         let cover = $(".textCover").val();
-        this.booksToAdd.push(new Book({
-            _id : 0,
-            author: author,
-            title: title,
-            numPages: numPages,
-            pubDate: pubDate,
-            cover: cover,
-            __v: 0
-        }));
         // clear the input tags to show the placeholder value
         $(".inpAuthor").val("");
         $(".inpTitle").val("");
         $(".inpNumPages").val("");
         $(".inpPubDate").val("");
         $(".textCover").val("");
-        if (this.findTitleInTable(title)) { // pop an alert but don't add the book to the table
+        // pop an alert if a duplicate title is found but don't add the book to the table
+        if ((this.homeTable.columns(2).data().eq(0).indexOf(title) == -1) ? false : true) { 
             alert("Duplicate title: " + title + " found in table");
-        } else {    // add the book to the modal table
+        } else if ((this.addTable.columns(1).data().eq(0).indexOf(title) == -1) ? false : true) { 
+            alert("Duplicate titles: " + title + " are not allowed");
+        } else {
+            // add the book to the add modal table and the booksToAdd array
             this.addTable.row.add([author, title, numPages, pubDate, cover.substring(0, 24)]);
             this.addTable.draw(false);
-        }
-    }
-
-    /*
-        findTitleInTable - looks for the title in the table
-            returns:
-                true - if duplicate is found
-                false - title is not in table
-    */
-    findTitleInTable(title) {
-        let index = this.homeTable.columns(2).data().eq( 0 ).indexOf( title );
-        if (index === -1 ) {
-            return false;
-        } else {
-            return true;
+            this.booksToAdd.push(new Book({
+                _id: 0,
+                author: author,
+                title: title,
+                numPages: numPages,
+                pubDate: pubDate,
+                cover: cover,
+                __v: 0
+            }));
         }
     }
 
@@ -137,20 +128,26 @@ class Library {
         btnSaveBooksToLibrary - Save all the books in booksToAdd array to the library
     */
     btnSaveBooksToLibrary() {
+        let _this = this;
         if (Array.isArray(this.booksToAdd) && this.booksToAdd.length) { // make sure there are books to add
-            let _this = this;
-            this.booksToAdd.forEach(function (book) {     // add books to library
-                _this.addBookToLibrary(book);
-            });
             // clear the table and the input fields in the add modal
             this.addTable.clear().draw();
             $(".inpAuthor").val("");
             $(".inpTitle").val("");
             $(".inpNumPages").val("");
             $(".inpPubDate").val("");
-            this.homeTable.draw(false);  //  Show the new books
             $("#addModal").modal("hide");
+            this.booksToAdd.forEach(function (book) {     // add books to library
+                _this.addBookToLibrary(book, addBookToTable);
+            });
+        } else {
+            console.log(this.booksToAdd);
         }
+        function addBookToTable(book) {
+            _this.homeTable.row.add(book);
+            _this.homeTable.row().draw(false);
+        }
+
     }
 
     /*
@@ -169,66 +166,63 @@ class Library {
 
     /*
         modalRecommend - Get a random book to recommend
-            returns: a book
     */
     modalRecommend() {
         let rowCount = this.homeTable.rows().count();
         let randomCount = Math.floor(Math.random() * rowCount);
         let book = new Book(this.homeTable.row(randomCount).data());
-        this.getBookById(book._id);
-        // $("#imgRecommendImage").attr("src", book.cover);
-        // $("#pRecommendTitle").text(book.title);
-        // $("#pRecommendAuthor").text(book.author);
-        // $("#pRecommendNumPages").text(book.numPages);
-        // $("#pRecommendPubDate").text(book.pubDate.toLocaleDateString("en-US", {
-        //     year: "numeric",
-        //     month: "long",
-        //     day: "numeric"
-        // }));
-    }
-
-    modalCallback(book) {
-        $("#imgRecommendImage").attr("src", book.cover);
-        $("#pRecommendTitle").text(book.title);
-        $("#pRecommendAuthor").text(book.author);
-        $("#pRecommendNumPages").text(book.numPages);
-        $("#pRecommendPubDate").text(book.pubDate.toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-        }));
+        this.getBookByIdFromLibrary(book._id, populateModalRecommend);
+        function populateModalRecommend(book) {
+            $("#imgRecommendImage").attr("src", book.cover);
+            $("#pRecommendTitle").text(book.title);
+            $("#pRecommendAuthor").text(book.author);
+            $("#pRecommendNumPages").text(book.numPages);
+            $("#pRecommendPubDate").text(book.pubDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            }));
+        }
     }
 
     /*
         btnSaveEdit - After finishing editing the book title and/or author save the book to the library
     */
     btnSaveEdit() {
+        let _this = this;
         this.bookToEdit.author = $("#editModalAuthor").val();
         this.bookToEdit.title = $("#editModalTitle").val();
-        this.updateBookInLibrary(this.bookToEdit);
-    }
+        this.updateBookInLibrary(this.bookToEdit, updateBookInTable);
 
-    btnAuthorToDelete(e) {
-        let author = $(e.currentTarget).text();
-        if (confirm("Are you sure you want to delete all the books by " + author + " ?")) {
-            $("#showAllAuthorsModal").modal("hide");
-            this.removeBooksByAuthor(author);
+        function updateBookInTable(book) {
+            // update the table
+            _this.editTableRow.children("td:nth-child(1)").text(book.author); 
+            _this.editTableRow.children("td:nth-child(2)").text(book.title);
+            _this.homeTable.draw(false);
+            $("#editModal").modal("hide");
         }
-        $("#showAllAuthorsModal").modal("hide");
-        this.homeTable.draw();  //  Refresh the table
     }
 
     /*
-        removeBooksByAuthor - 
+        removeBooksByAuthor - Removes all books by an author from library and main table 
     */
-    removeBooksByAuthor(author) {
+   removeBooksByAuthor(e) {
         let _this = this;
-        let rows = this.homeTable.rows( function ( idx, data ) {
-            return (data.author === author) ? true : false;
-        } ).data();
-        for (let i=0; i<rows.length; ++i) {
-            let book = new Book(rows[i]);
-            _this.removeBookById(book._id);
+        let author = $(e.currentTarget).text();
+        if (confirm("Are you sure you want to delete all the books by " + author + " ?")) {
+            $("#showAllAuthorsModal").modal("hide");
+            let rows = this.homeTable.rows(function (idx, data) {
+                return (data.author === author) ? true : false;
+            }).data().each( function (row) {
+                let book = new Book(row);
+                _this.removeBookByIdFromLibrary(book._id, removeBookByIdFromTable);
+            });
+        }
+        // remove book from the main table by book._id
+        function removeBookByIdFromTable(_id) {
+            _this.homeTable.rows( function ( idx, data, node ) {
+                return ((data._id == _id) ? true : false);
+            } ).remove().draw();
         }
     }
 
@@ -245,65 +239,60 @@ class Library {
     }
 
     /*
-        updateBookInTable - update the author and title of the book in the table
-    */
-    updateBookInTable(book) {
-        this.editTableRow.children("td:nth-child(1)").text(book.author); // update the table
-        this.editTableRow.children("td:nth-child(2)").text(book.title);
-        this.homeTable.draw(false);
-        $("#editModal").modal("hide");
-    }
-
-    /*
         iconDeleteRow - delete the selected row from the table and the corresponding book from the library
     */
     iconDeleteRow(e) {
+        let _this = this;
         let tableRow = $(e.currentTarget).closest("tr");
         let book = $("#displayTable").dataTable().fnGetData( tableRow );   // get the data for the row
         if (confirm("Are you sure you want to delete \"" + book.title + "\" ?")) {
-            this.removeBookById(book._id);
-            this.homeTable.row(tableRow).remove();
-            this.homeTable.draw(false);
+            this.removeBookByIdFromLibrary(book._id, removeRow);
+        }
+        function removeRow() {
+            _this.homeTable.row(tableRow).remove();
+            _this.homeTable.draw(false);
         }
     }
 
     /*
-        getBookById - get the book from the library 
+        getBookById - get the book by Id from the library 
     */
-    getBookById(id) {
+    getBookByIdFromLibrary(id, callback) {
         let _this = this;
         $.ajax ({
             dataType: "json",
             type: "GET",
             url: "http://localhost:3000/library/" + id
         }).done( function(response) {
-            _this.modalCallback(new Book(response));
+            callback(new Book(response));
         }).fail( function(response) {
             _this.failedResponse(response);
         });
     }
 
     /*
-        removeBookById - removes a book from the database then calls removeBookFromTable on return
+        removeBookByIdFromLibrary - removes a book by Id from the library
     */
-    removeBookById(id) {
+    removeBookByIdFromLibrary(_id, callback) {
         let _this = this;
         $.ajax ({
-            url: "http://localhost:3000/library/" + id,
+            url: "http://localhost:3000/library/" + _id,
             dataType: "json",
             type: "DELETE",
         }).done( function(response) {
-            let book = new Book(response);
-            _this.removeBookFromTable(book);
+            // console.log("delete success response")
+            callback(_id);
         }).fail( function(response) {
-            _this.failedResponse(response);
+            // console.log("delete fail response")
+            callback(_id);
+            // _this.failedResponse(response);
         });
     }
 
     /*
-        getLibraryFromDB - will get the entire library from the database
+        getAllBooksFromLibrary - will get all the books in the library
     */
-    getLibraryFromDB() {
+    getAllBooksFromLibrary() {
         let _this = this;
         $.ajax ({
             dataType: "json",
@@ -311,7 +300,9 @@ class Library {
             url: "http://localhost:3000/library/"
         }).done( function (response) {
             response.forEach( function (object) {
-                _this.addBookToTable(new Book(object));
+                let book = new Book(object);
+                _this.homeTable.row.add(book);
+                _this.homeTable.row().draw(false);
             });
         }).fail( function (response) {
             _this.failedResponse(response);
@@ -319,31 +310,9 @@ class Library {
     }
 
     /*
-        addBookToTable - adds a book to the table 
-    */
-    addBookToTable(book) {
-        this.homeTable.row.add(book);
-        this.homeTable.row().draw(false);
-    }
-
-    /*
-        removeBookFromTable - removes a book from the table
-    */
-    removeBookFromTable(book) {
-        this.homeTable.column( function ( index, data, node) {
-            return ( data._id === book._id) ? true : false;
-        }).remove();
-        this.homeTable.draw(false);
-    }
-
-    failedResponse(response) {
-        console.log(response);
-    }
-
-    /*
         updateBookInLibrary - updates a book in the library
     */
-    updateBookInLibrary(book) {
+    updateBookInLibrary(book, callback) {
         let _this = this;
         $.ajax ({
             dataType: "json",
@@ -351,7 +320,7 @@ class Library {
             data: book,
             url: "http://localhost:3000/library/" + book._id,
         }).done( function (response) {
-            _this.updateBookInTable(new Book(response));       // The response will have the _id field
+            callback(new Book(response));
         }).fail( function (response) {
             _this.failedResponse(response);
         });
@@ -361,7 +330,7 @@ class Library {
         addBookToLibrary - add the book to the library
             book - book to add
     */
-    addBookToLibrary(book) {
+    addBookToLibrary(book, callback) {
         let _this = this;
         $.ajax ({
             dataType: "json",
@@ -369,10 +338,15 @@ class Library {
             data: book,
             url: "http://localhost:3000/library/",
         }).done( function (response) {
-            _this.addBookToTable(new Book(response));       // The response will have the _id field
+            // The response will have a new book oobject with _id
+            callback(new Book(response));      
         }).fail( function (response) {
             _this.failedResponse(response);
         });
+    }
+
+    failedResponse(response) {
+        console.log(response);
     }
 }
 
